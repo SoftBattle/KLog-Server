5dbdec51824ce78c04e9c95d78e1eee928277e90
ygx <1241749920@qq.com>
2021-09-22 09:02:32 +0800

    初步搭建

---
+++ src/test/java/org/softbattle/klog_server/KlogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.softbattle.klog_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
