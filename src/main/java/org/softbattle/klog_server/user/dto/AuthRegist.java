e2c6c64ba9498185d67bfcad86e34eb317716fc4
ygx <1241749920@qq.com>
2021-10-12 23:04:38 +0800

    用户注册dto

---
+++ src/main/java/org/softbattle/klog_server/user/dto/AuthRegist.java
@@ -0,0 +1,39 @@
+package org.softbattle.klog_server.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import static org.softbattle.klog_server.user.service.serviceimpl.UserServiceImpl.DEFAULTAVATAR;
+import static org.softbattle.klog_server.user.service.serviceimpl.UserServiceImpl.DEFAULTNICKNAME;
+
+/**
+ * 用户注册返回结果类
+ * @author ygx
+ */
+@Data
+@AllArgsConstructor
+public class AuthRegist {
+    /**
+     * 用户名
+     */
+    private String uid;
+    /**
+     * 昵称
+     */
+    private String nickname;
+    /**
+     * token
+     */
+    private String token;
+    /**
+     * 头像地址
+     */
+    private String avatar;
+
+    public AuthRegist(String uid, String token){
+        this.uid = uid;
+        this.token = token;
+        this.nickname = DEFAULTNICKNAME;
+        this.avatar = DEFAULTAVATAR;
+    }
+}
