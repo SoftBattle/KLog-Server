f17ea9b286575cd628f7dd7e24ac321e1b497bc0
pengsunsp <1147464090@qq.com>
2021-10-12 18:15:22 +0800

    新写了一些逻辑

--- src/main/java/org/softbattle/klog_server/article/controller/ArticleController.java
+++ src/main/java/org/softbattle/klog_server/article/controller/ArticleController.java
@@ -102,7 +102,9 @@ public class ArticleController {
         return JSON.toJSONString(map);
     }
 
-    /**  这个没写好，不知道什么意思
+    /**
+     * 浏览文章
+     * 这个没写好，不知道什么意思
      *   后台需要在在url中取得参数pid，以此返回对应的文章
      * @param pid
      * @return
@@ -110,11 +112,84 @@ public class ArticleController {
     @RequestMapping(value = "/pid",method = RequestMethod.GET)
     public String lookPost(String pid){
         Map<String, Object> map = new HashMap<>();
-        Article article = articleService.lookPost(pid);
-        Integer articleId = article.getArticleId();
+        try {
+            Map<String, Object> data = new HashMap<>();
+            Article article = articleService.lookPost(pid);
+            Integer articleId = article.getArticleId();//文章id
+            String uid = article.getArthor();//作者id
+            /**接下来根据这个作者id来获得
+             nickname: 'cmkangkang',
+             avatar: '/demo.png',
+             follow: false // 是否关注了该作者
+             然后装在一个叫做author的map里面
+             **/
+            Map<String, Object> author = new HashMap<>();
+            author.put("uid", uid);
+            //接下来就把别的放进来
 
+
+            String title = article.getTitle();//
+            String subTitle = article.getSubTitle();
+            String content = article.getContent();
+            String banner = article.getBanner();//图片的数组？ 肯定不能这样返回的
+            String tags = article.getTags();
+            String views = article.getViews();//不明白为啥是string类型的？好奇怪
+            LocalDate ctim = article.getCtim();
+            //mtime没有这个属性，自己乱加的
+
+            //star是否被收藏，需要从作者表里面读，判断这个articleID是不是在那个数组里面
+
+            //然后再把这个所有装在data的一个map里面
+            data.put("pid", articleId);
+            data.put("author", author);
+            data.put("title", title);
+            data.put("subTitle", subTitle);
+            data.put("content", content);
+            data.put("banner", banner);
+            data.put("tags", tags);
+            data.put("views", views);
+            data.put("ctim", ctim);
+            data.put("star", "  ");//是否被收藏
+
+            map.put("stat","ok");
+            map.put("data",data);
+        }
+        catch (Exception e){
+            map.put("stat", "Internal_Server_Error");
+            map.put("msg","内部错误");
+        }
+        return JSON.toJSONString(map);
+    }
+
+
+    @RequestMapping(value = "/pid",method = RequestMethod.PUT)
+    public String updateArticle(String title, String subTitle, String[] banners, String[] tags, String content){
+        Map<String, Object> map = new HashMap<>();
+        try {
+
+            //解析token得到作者，判断是否有权限；在url中获取pid，找到需要更新的的文章。
+
+        }
+        catch (Exception e){
+            map.put("stat", "Internal_Server_Error");
+            map.put("msg","内部错误");
+        }
 
         return JSON.toJSONString(map);
     }
 
+    @RequestMapping(value = "/pid",method = RequestMethod.DELETE)
+    public String deleteArticle(String pid){
+        Map<String, Object> map = new HashMap<>();
+        try {
+            //解析token得到作者，在url中获取pid，判断是否具有权限删除，能则删除。
+
+        }
+        catch (Exception e){
+            map.put("stat", "Internal_Server_Error");
+            map.put("msg","内部错误");
+        }
+
+        return JSON.toJSONString(map);
+    }
 }
